539ee982ea50fa138da93b239a028ffa4cf2082b
Zoya <popyvanovaz@mail.ru>
2021-12-20 17:38:30 +0300

    formatting has been performed

--- src/main/java/ru/geekbrains/Homework3/Sample01.java
+++ src/main/java/ru/geekbrains/Homework3/Sample01.java
@@ -10,19 +10,13 @@ public class Sample01 {
         int min = 0;
         int max = 9;
 
-
         getNumberFromScanner("Угадайте число от 0 до 9. У вас 3 попытки", 0, 9, 3);
         sc.close();
-
-
-
     }
 
         public static void getNumberFromScanner(String message, int min, int max, int maxTryCount) {
 
             System.out.println(message);
-
-
             int randomNumber = (int)(Math.random() * (max + 1));
 
           for (int tryCount = 1; tryCount <= maxTryCount; tryCount++){
@@ -30,19 +24,17 @@ public class Sample01 {
               if (number == randomNumber){
                   System.out.println("Вы угадали! Попробуете еще раз? Введите 1, если хотите проддолжить и 0, если хотите выйти: ");
                   aNewAttempt();
-          } else if (number > randomNumber){
-              System.out.println("Вы ввели слишком большое число");
+              }else if (number > randomNumber){
+                      System.out.println("Вы ввели слишком большое число");
+              }else if (number < randomNumber){
+                 System.out.println("Вы ввели слишком маленькое число");
               }
-          else if (number < randomNumber){
-             System.out.println("Вы ввели слишком маленькое число");
-          }
-          if (tryCount == maxTryCount) {
-              System.out.println("Вы проиграли. Правильный ответ: " + randomNumber + " Попробуете снова? Введите 1, если хотите проддолжить и 0, если хотите выйти: ");
-              aNewAttempt();
+              if (tryCount == maxTryCount) {
+                  System.out.println("Вы проиграли. Правильный ответ: " + randomNumber + " Попробуете снова? Введите 1, если хотите проддолжить и 0, если хотите выйти: ");
+                  aNewAttempt();
+              }
           }
         }
-        }
-
     public static void aNewAttempt () {
         int options = sc2.nextInt();
         switch (options){
@@ -55,5 +47,4 @@ public class Sample01 {
         }
         sc2.close();
     }
-
 }
